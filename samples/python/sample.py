777a4776100e9662c9914f886387549b35ab3573
BhumikaGanta <gbhumika@actalentservices.com>
2026-06-03 17:26:09 +0530

    Update sample.py

--- samples/python/sample.py
+++ samples/python/sample.py
@@ -1,8 +1,11 @@
 # Sample Python test file
 # This file is added to demonstrate the Python sample folder structure.
 
-def say_hello():
-    print('Hello from Python sample file')
 
-if __name__ == '__main__':
+def say_hello():
+    print("Hello from Python sample file")
+
+
+if __name__ == "__main__":
     say_hello()
+    
