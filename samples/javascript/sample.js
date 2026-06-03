2c6d4c2263abcd6975d64fbe5637c185b0a7b78b
BhumikaGanta <gbhumika@actalentservices.com>
2026-06-03 17:26:37 +0530

    Update sample.js

--- samples/javascript/sample.js
+++ samples/javascript/sample.js
@@ -2,7 +2,7 @@
 // This file is added to demonstrate the JavaScript sample folder structure.
 
 function sayHello() {
-  console.log('Hello from JavaScript sample file');
+  console.log("Hello from JavaScript sample file");
 }
 
 sayHello();
