dadcae87911f960c4df789e9907bbb7a4750d7f7
BHUMIKAGANTA <gbhumika@actalentservices.com>
2026-06-03 17:07:16 +0530

    Add sample test files for C++, JavaScript, and Python

---
+++ samples/javascript/sample.js
@@ -0,0 +1,8 @@
+// Sample JavaScript test file
+// This file is added to demonstrate the JavaScript sample folder structure.
+
+function sayHello() {
+  console.log('Hello from JavaScript sample file');
+}
+
+sayHello();
---
+++ samples/python/sample.py
@@ -0,0 +1,8 @@
+# Sample Python test file
+# This file is added to demonstrate the Python sample folder structure.
+
+def say_hello():
+    print('Hello from Python sample file')
+
+if __name__ == '__main__':
+    say_hello()
