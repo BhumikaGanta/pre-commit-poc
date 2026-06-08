b97495c3c30451f8d5d32b473c4d337afa4d2deb
Divyashree H R <dhr@BLR.ALLEGISINDIA.COM>
2026-06-08 16:40:40 +0530

    Clang-format check

--- samples/cpp/sample.cpp
+++ samples/cpp/sample.cpp
@@ -2,8 +2,8 @@
 // This file is added to demonstrate the C++ sample folder structure.
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello from C++ sample file\n";
-    return 0;
+int main() {
+  std::cout << "Hello from C++ sample
+      file\n ";
+      return 0;
 }
