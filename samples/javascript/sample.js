13317f5798caa9a96de7639c850db721327d8b39
Divyashree H R <dhr@BLR.ALLEGISINDIA.COM>
2026-06-12 18:40:54 +0530

    JS Local negative test

--- samples/javascript/sample.js
+++ samples/javascript/sample.js
@@ -1,13 +1,5 @@
-// Sample JavaScript test file
-// This file is added to demonstrate the JavaScript sample folder structure.
-
-function sayHello() {
-  console.log("Hello from JavaScript sample file");
+function greet() {
+  console.log(message);
 }
-
-sayHello();
-
-function test() {
-  console.log("Testing eslint hook");
-}
-test();
+ 
+greet();
