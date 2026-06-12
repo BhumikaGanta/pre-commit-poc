f55c28878bf4dd839d341d6d220be0489b357d33
Divyashree H R <dhr@BLR.ALLEGISINDIA.COM>
2026-06-12 12:52:22 +0530

    test python positive hook

--- samples/python/sample.py
+++ samples/python/sample.py
@@ -1,9 +1,8 @@
 # Sample Python test file
-# This file is added to demonstrate the Python sample folder structure.
 
 
 def say_hello():
-    print("Hello from Python sample file")
+    print("Hello from Python sample files")
 
 
 if __name__ == "__main__":
